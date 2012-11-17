88d9e90f322f788add25aab7caf663d96e286bbc
Mohammad Reza Shariatzadeh <Mrshcom@gmail.com>
2012-11-18 01:55:25 +0330

    Optimizing script codes!

--- script/script.js
+++ script/script.js
@@ -1,12 +1,18 @@
 $(function(){
 	
-	var li = $('section.container section.leftside > nav > menu > li'),
-		content = $('section.container section.content-container section.main-holder'),
+	var content = $('section.container section.content-container section.main-holder'),
+		content_page = $('section.main-holder > article.content-holder div'),
+		li = $('section.container section.leftside > nav > menu > li'),
+		img = $('section.main-holder > figure.img-holder img'),
 		logo = $('section.container div.logo'),
+		easein = 'easeOutBounce',
+		easeout = 'easeInOutQuad',
+		currentimg = 0;
 		due = 800,
 		delay = 400,
 		step = 300;
 
+	// for Load Effect
 	setTimeout(function(){
 		logo.animate({
 			opacity:1
@@ -41,61 +47,31 @@ $(function(){
 	// },(delay+400));
 
 
-
-
-
-	var img = $('section.main-holder > figure.img-holder img'),
-			content_page = $('section.main-holder > article.content-holder div'),
-			link = $('section.container section.leftside > nav > menu > li'),
-			easein = 'easeOutBounce',
-			easeout = 'easeInOutQuad',
-			// ease = !!window.Zepto ? 'ease-in-out' : 'easeOutBounce',
-			currentimg = 0;
-
-
+	// for Change Page with Effect and Change Hash Page
 	var loadpage = function(url){
 		url = url.length > 2 ? url.substr(1) : 'index.html';
-		// $('.main-content').load(url+' .main-content');
-		// alert(url);
 	}
-
-	// var animate = function(elm,current,nw,top){
-	// 	elm.eq(current).animate({
-	// 		'top': top + 'px',
-	// 		'opacity':'0'
-	// 	},500,easeout,function(){
-	// 		$(this).css({'top' : '-' + top + 'px'});
-	// 	});
-
-	// 	elm.eq(nw).animate({
-	// 		'top':'0',
-	// 		'opacity':'1'
-	// 	},1000,easein);				
-	// }
 
 	var slide = function(n){
 		if(currentimg==n) return;
 		img.eq(currentimg).animate({
-			'top':'200px',
+			'top':'50px',//200
 			'opacity':'0'
 		},300,easeout,function(){
-			$(this).css('top','-280px');
+			$(this).css('top','-50px'); //-280
 		});
 
 		img.eq(n).animate({
 			'top':'0',
 			'opacity':'1'
-		},600,easein);	
-
-		// animate(img,currentimg,n,280);
-		// animate(content,currentimg,n,-380);
+		},900,easein);	
 
 		content_page.eq(currentimg).animate({
-			'top':'-400px',
+			'top':'-100px',//-400
 			'opacity':'0'
 		},300,easeout,function(){
 			$(this).css({
-				'top':'400px',
+				'top':'100px', //400
 				'display':'none'
 			});
 		});
@@ -105,10 +81,9 @@ $(function(){
 		}).animate({
 			'top':'0',
 			'opacity':'1'
-		},600,easein);
+		},900,easein);
 		currentimg = n;
 	}
-
 
 	var pageUrl = window.location.href;
 	sharPos = pageUrl.indexOf('#');
@@ -118,35 +93,23 @@ $(function(){
 		// alert('pageUrl' + pageUrl);
 	}
 
-	// var pagenum;
 	$('section.leftside a').click(function(){
 		var url = $(this).attr('href');
-		// x = $(this).parent().index();
-		// slide(x);
-		// alert('a click index:'+x);
 		window.location.href = pageUrl + '#' + url;
-		//$('.main-content').load(url+' .main-content');
 		return false;
 	});
 
 	var pagenum;
 	window.onhashchange = function(){
 		loadpage(window.location.hash);
-		// alert(window.location.hash);
 		pagenum = window.location.hash;
 
-		// alert(x);
 		y = pagenum.indexOf('.');
 		pagenum = pagenum.substr(y-1);
 		pagenum = pagenum.substr(0,1);
 		// alert('hash index:'+pagenum);
 		// alert(x);
 		slide(pagenum-1);
-		// slide(pagenum);
 	}
-
-
-
-
 	
 });
