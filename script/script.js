fd0c418f7d255c703298480841b5fa5a318904a8
Mohammad Reza Shariatzadeh <Mrshcom@gmail.com>
2012-11-17 12:49:21 +0330

    Script for Effect of main nav!

--- script/script.js
+++ script/script.js
@@ -1,5 +1,22 @@
 $(function(){
 	
-	
+	var li = $('section.container section.leftside > nav > menu > li'),
+		due = 800,
+		delay = 200,
+		step = 300;
+
+
+	li.each(function() {
+        var that = this;
+		setTimeout(function(){
+			$(that).animate({
+				left:'30px'	
+			},due,'easeOutBack');
+		},delay);
+		delay += step;
+    });
+
+
+
 	
 });
