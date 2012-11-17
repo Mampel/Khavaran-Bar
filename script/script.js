849bab6f7b633e30f343cc9f0bf7213422a708f9
Mohammad Reza Shariatzadeh <Mrshcom@gmail.com>
2012-11-17 17:22:39 +0330

    Optimizing Codes!

--- script/script.js
+++ script/script.js
@@ -1,17 +1,18 @@
 $(function(){
 	
 	var li = $('section.container section.leftside > nav > menu > li'),
-		content = $('section.container section.content-container'),
+		content = $('section.container section.content-container section.main-holder'),
 		logo = $('section.container div.logo'),
 		due = 800,
-		delay = 1200,
+		delay = 400,
 		step = 300;
 
 	setTimeout(function(){
 		logo.animate({
 			opacity:1
-		},700);
-	},400);
+		},due,'easeOutBack');
+	},delay); //400
+	delay += 500;
 
 	li.each(function() {
         var that = this;
@@ -23,18 +24,18 @@ $(function(){
 		delay += step;
     }).mouseover(function(){
 		$(this).animate({
-				left:20
-			},200,'easeOutExpo');
+			left:20
+		},200,'easeOutExpo');
 	}).mouseout(function(){
 		$(this).animate({
-				left:0
-			},500,'easeOutExpo');
+			left:0
+		},500,'easeOutExpo');
 	});
 
 	setTimeout(function(){
-		content.animate({
+		content.css('display','block').animate({
 			opacity:1
-		},1400);
-	},3400);
+		},1400,'easeOutBack');
+	},(delay+400));
 	
 });
