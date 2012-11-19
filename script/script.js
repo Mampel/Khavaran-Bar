36d6382fa1b2a1fcee247e42ef45de9f6db48754
Mohammad Reza Shariatzadeh <Mrshcom@gmail.com>
2012-11-20 02:06:21 +0330

    Change name of img!

--- script/script.js
+++ script/script.js
@@ -114,12 +114,12 @@ $(function(){
 	// Change Template
 	$('span.blue').click(function(){
 		$('section.background div.backimg').addClass('blue').removeClass('red');
-		$('section.main-holder > figure.img-holder img.img1').attr("src","images/home-b.jpg");
+		$('section.main-holder > figure.img-holder img.img1').attr("src","images/home-b.png");
 		$('section.main-holder article.content-holder *').addClass('txt-blue').removeClass('txt-red');
 	});
 	$('span.red').click(function(){
 		$('section.background div.backimg').addClass('red').removeClass('blue');
-		$('section.main-holder > figure.img-holder img.img1').attr("src","images/home-r.jpg");
+		$('section.main-holder > figure.img-holder img.img1').attr("src","images/home-r.png");
 		$('section.main-holder article.content-holder *').addClass('txt-red').removeClass('txt-blue');
 	});
 
